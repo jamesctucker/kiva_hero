4f1e1a8936600a79aca317ee7bf875cd0f281898
James Tucker <jamesctucker@protonmail.com>
2019-04-29 10:17:38 -0500

    restyled nav header links and color

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -12,22 +12,9 @@ import Lend from './../Lending/Lend';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Now
-        </a>
-      </header>
+    <div>
       <Router>
+        <Nav />
         <div>
           <Route exact path="/" component={Home} />
           <Route exact path="/about" component={About} />
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import './Nav.css';
 
 class Nav extends Component {
-    render()
-    <div>
-    <Link to="/lend">
-        <h2 className="nav-title">Lend</h2>
-    </Link>
-    <Link to="/about">
-        <h2 className="nav-title">About</h2>
-    </Link>
-</div>
+    render() {
+        return (
+            <div className="nav">
+                <Link to="/">
+                    <h2 className="nav-title">Kiva Hero</h2>
+                </Link>
+                <div className="nav-right">
+                    <Link to="/lend">
+                        <h2 className="nav-link">Lend</h2>
+                    </Link>
+                    <Link to="/about">
+                        <h2 className="nav-link">About</h2>
+                    </Link>
+                </div>
+            </div>
+        )
+    }
 }
 
 
