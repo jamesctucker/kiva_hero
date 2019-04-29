7def949a18e471e59fb3b7e4666e4ac84fce90d3
James Tucker <jamesctucker@protonmail.com>
2019-04-29 13:43:22 -0500

    graphql query successfully retrieving test data from kiva

--- src/components/Lending/Lend.js
+++ src/components/Lending/Lend.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import LendingProfiles from './LendingProfiles';
 
 class Lend extends Component {
     render() {
         return (
             <div>
                 <h1>Lend</h1>
+                <LendingProfiles />
             </div>
         )
     }
--- src/components/Lending/LendingProfiles.js
+++ src/components/Lending/LendingProfiles.js
@@ -2,6 +2,57 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class LendingProfiles extends Component {
+    state = {
+        error: null,
+        isLoaded: false,
+        items: []
+    }
+
+    componentDidMount() {
+
+        // This is the GraphQL query
+        const query = `
+        query {
+            lend {
+              loans(filters: {status: fundraising}, sortBy: newest) {
+                values {
+                  id
+                }
+              }
+            }
+          }
+        `;
+
+        // These variables are optional, leave empty for now
+        const variables = {};
+
+        // We call the method here to execute our async function
+        this.getLoanItems(query, variables)
+
+    }
+
+    getLoanItems = async (query, variables) => {
+        try {
+            const response = await axios.post('https://api.kivaws.org/graphql', {
+                variables,
+                query
+            });
+
+            // Log the response so we can look at it in the console
+            console.log(response.data)
+
+            // Set the data to the state
+            this.setState(() => ({
+                isLoaded: true,
+                items: response.data.data
+            }));
+
+        } catch (error) {
+            // If there's an error, set the error to the state
+            this.setState(() => ({ error }))
+        }
+    }
+
     render() {
         return (
             <div>
