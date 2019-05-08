1ab3b0b982ce98ebb1551367ed517fa982b99fd7
James Tucker <jamesctucker@protonmail.com>
2019-05-08 13:59:44 -0500

    moved country data to a json-bin api; brought in the data via axios to clean up the code

--- src/views/Lending/LendingProfiles.js
+++ src/views/Lending/LendingProfiles.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import gql from "graphql-tag";
 import Countdown from 'react-countdown-now';
 import { Query } from "react-apollo";
@@ -6,7 +7,6 @@ import { Query } from "react-apollo";
 import './Lending.css';
 
 import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
@@ -19,8 +19,6 @@ import Link from '@material-ui/core/Link';
 import Divider from '@material-ui/core/Divider';
 
 import Female_Clothier from './woman-clothier.jpg';
-
-
 
 
 const kivaLoans = gql`
@@ -45,318 +43,6 @@ const kivaLoans = gql`
   }
 }`;
 
-const countries = [
-    {
-        "name": "Albania",
-        "value": "AL"
-    },
-    {
-        "name": "Armenia",
-        "value": "AM"
-    },
-    {
-        "name": "Bangladesh",
-        "value": "BD"
-    },
-    {
-        "name": "Belize",
-        "value": "BZ"
-    },
-    {
-        "name": "Benin",
-        "value": "BJ"
-    },
-    {
-        "name": "Bhutan",
-        "value": "BT"
-    },
-    {
-        "name": "Bolivia",
-        "value": "BO"
-    },
-    {
-        "name": "Brazil",
-        "value": "BR"
-    },
-    {
-        "name": "Burkina Faso",
-        "value": "BF"
-    },
-    {
-        "name": "Cambodia",
-        "value": "KH"
-    },
-    {
-        "name": "Cameroon",
-        "value": "CM"
-    },
-    {
-        "name": "Canada",
-        "value": "CA"
-    },
-    {
-        "name": "Colombia",
-        "value": "CO"
-    },
-    {
-        "name": "Congo (DRC)",
-        "value": "CD"
-    },
-    {
-        "name": "Costa Rica",
-        "value": "CR"
-    },
-    {
-        "name": "Cote D'Ivoire",
-        "value": "CI"
-    },
-    {
-        "name": "Dominican Republic",
-        "value": "DO"
-    },
-    {
-        "name": "Ecuador",
-        "value": "EC"
-    },
-    {
-        "name": "Egypt",
-        "value": "EG"
-    },
-    {
-        "name": "El Salvador",
-        "value": "SV"
-    },
-    {
-        "name": "Fiji",
-        "value": "FJ"
-    },
-    {
-        "name": "Georgia",
-        "value": "GE"
-    },
-    {
-        "name": "Ghana",
-        "value": "GH"
-    },
-    {
-        "name": "Guam",
-        "value": "GU"
-    },
-    {
-        "name": "Guatemala",
-        "value": "GT"
-    },
-    {
-        "name": "Haiti",
-        "value": "HT"
-    },
-    {
-        "name": "Honduras",
-        "value": "HN"
-    },
-    {
-        "name": "India",
-        "value": "IN"
-    },
-    {
-        "name": "Indonesia",
-        "value": "ID"
-    },
-    {
-        "name": "Israel",
-        "value": "IL"
-    },
-    {
-        "name": "Jordan",
-        "value": "JO"
-    },
-    {
-        "name": "Kenya",
-        "value": "KE"
-    },
-    {
-        "name": "Kosovo",
-        "value": "XK"
-    },
-    {
-        "name": "Kyrgyzstan",
-        "value": "KG"
-    },
-    {
-        "name": "Lao",
-        "value": "LA"
-    },
-    {
-        "name": "Lebanon",
-        "value": "LB"
-    },
-    {
-        "name": "Lesotho",
-        "value": "LS"
-    },
-    {
-        "name": "Liberia",
-        "value": "LR"
-    },
-    {
-        "name": "Madagascar",
-        "value": "MG"
-    },
-    {
-        "name": "Malawi",
-        "value": "MW"
-    },
-    {
-        "name": "Mali",
-        "value": "ML"
-    },
-    {
-        "name": "Mexico",
-        "value": "MX"
-    },
-    {
-        "name": "Moldova",
-        "value": "MD"
-    },
-    {
-        "name": "Mozambique",
-        "value": "MZ"
-    },
-    {
-        "name": "Myanmar",
-        "value": "MM"
-    },
-    {
-        "name": "Namibia",
-        "value": "NA"
-    },
-    {
-        "name": "Nepal",
-        "value": "NP"
-    },
-    {
-        "name": "Nicaragua",
-        "value": "NI"
-    },
-    {
-        "name": "Nigeria",
-        "value": "NG"
-    },
-    {
-        "name": "Pakistan",
-        "value": "PK"
-    },
-    {
-        "name": "Palestine",
-        "value": "PS"
-    },
-    {
-        "name": "Panama",
-        "value": "PA"
-    },
-    {
-        "name": "Papua New Guinea",
-        "value": "PG"
-    },
-    {
-        "name": "Paraguay",
-        "value": "PY"
-    },
-    {
-        "name": "Peru",
-        "value": "PE"
-    },
-    {
-        "name": "Philippines",
-        "value": "PH"
-    },
-    {
-        "name": "Puerto Rico",
-        "value": "PR"
-    },
-    {
-        "name": "Rwanda",
-        "value": "RW"
-    },
-    {
-        "name": "Samoa",
-        "value": "WS"
-    },
-    {
-        "name": "Senegal",
-        "value": "SN"
-    },
-    {
-        "name": "Sierra Leone",
-        "value": "SL"
-    },
-    {
-        "name": "Solomon Islands",
-        "value": "SB"
-    },
-    {
-        "name": "South Africa",
-        "value": "ZA"
-    },
-    {
-        "name": "South Sudan",
-        "value": "SS"
-    },
-    {
-        "name": "Tajikistan",
-        "value": "TJ"
-    },
-    {
-        "name": "Tanzania",
-        "value": "TZ"
-    },
-    {
-        "name": "Thailand",
-        "value": "TH"
-    },
-    {
-        "name": "Timor-Leste",
-        "value": "TL"
-    },
-    {
-        "name": "Togo",
-        "value": "TG"
-    },
-    {
-        "name": "Tonga",
-        "value": "TO"
-    },
-    {
-        "name": "Turkey",
-        "value": "TR"
-    },
-    {
-        "name": "Uganda",
-        "value": "UG"
-    },
-    {
-        "name": "United States",
-        "value": "US"
-    },
-    {
-        "name": "Vietnam",
-        "value": "VN"
-    },
-    {
-        "name": "Virgin Islands",
-        "value": "VI"
-    },
-    {
-        "name": "Zambia",
-        "value": "ZM"
-    },
-    {
-        "name": "Zimbabwe",
-        "value": "ZW"
-    }
-];
-
-
 
 const renderer = ({ hours, minutes, seconds }) => {
     return <div className="timer-div">
@@ -368,19 +54,29 @@ const renderer = ({ hours, minutes, seconds }) => {
 }
 
 
-
-
 class LendingProfiles extends Component {
     constructor(props) {
         super(props)
         this.state = {
+            countries: [],
             sort: 'expiringSoon',
             country: " ",
             limitResults: 10
         }
     }
 
+    componentDidMount() {
+        axios
+            .get('https://api.jsonbin.io/b/5cd324de64d4fc359ead9996/2')
+            .then(response => {
+                this.setState({
+                    countries: response.data,
+                });
+                console.log(response.data);
+            })
+            .catch(error => this.setState({ error }));
 
+    }
 
 
     handleChange = event => {
@@ -388,9 +84,8 @@ class LendingProfiles extends Component {
     };
 
 
-
     render() {
-        const { country, sort, limitResults } = this.state;
+        const { country, sort, limitResults, countries } = this.state;
         return (
             <Query query={kivaLoans} variables={{ country: country, sortBy: sort, limit: limitResults }}>
                 {({ data, loading, error }) => {
